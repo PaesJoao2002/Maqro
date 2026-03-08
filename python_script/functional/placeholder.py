b143af7d686ed8082505e3487d7fbdc7cd8a64cf
PaesJoao2002 <jvpaes2002@gmail.com>
2026-03-08 04:15:10 -0300

    rafsgsagh

--- python_script/functional/placeholder.py
+++ python_script/functional/placeholder.py
@@ -2,7 +2,7 @@ import customtkinter as ctk
 
 class PlaceholderTextManager:
     def __init__(self, entry: ctk.CTkEntry):
-        self.entry = entry
+        self.text_entry = entry
         self.placeholder_text = ""
         self.active = False
 
@@ -11,25 +11,25 @@ class PlaceholderTextManager:
         self._apply_placeholder_text()
 
     def _apply_placeholder_text(self):
-        self.entry.delete(0, "end")
-        self.entry.insert(0,
+        self.text_entry.delete(0, "end")
+        self.text_entry.insert(0,
                           self.placeholder_text)
-        self.entry.configure(text_color="gray")
+        self.text_entry.configure(text_color="gray")
         self.active = True
-        self.entry.icursor(0)
-        self.entry.xview_moveto(0)
+        self.text_entry.icursor(0)
+        self.text_entry.xview_moveto(0)
 
     def focus_in(self, event=None):
         if self.active:
-            self.entry.icursor(0)
-            self.entry.xview_moveto(0)
+            self.text_entry.icursor(0)
+            self.text_entry.xview_moveto(0)
 
     def restore(self, event=None):
-        if not self.entry.get():
+        if not self.text_entry.get():
             self._apply_placeholder_text()
 
     def clear_if_active(self):
         if self.active:
-            self.entry.delete(0, "end")
-            self.entry.configure(text_color=("#FFFFFF"))
+            self.text_entry.delete(0, "end")
+            self.text_entry.configure(text_color=("#FFFFFF"))
             self.active = False
